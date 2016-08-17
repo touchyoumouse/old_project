4288bf698ba86e262b613c904788711f4d91e205
feng2016 <ablock@163.com>
2016-08-17 10:21:11 +0800

    librtmp version-1.0

--- screen_to_rtmp/RtmpLiveScreen_mp3/base/Base.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BASE_H_
-#define _BASE_H_
-
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-    TypeName(const TypeName&);               \
-    void operator=(const TypeName&)
-
-#endif  // _BASE_H_
